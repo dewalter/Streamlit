5584760282dc73216ca28b9e218817a52d8ad997
Walter <wkrijt@gmail.com>
2023-03-31 12:17:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,12 @@ st.text ('Hello everybody, my first text field in streamlit')
 col1, col2 = st.columns(2)
 col1.write('# This is Column 1')
 col2.write('# This is Column 2')
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
