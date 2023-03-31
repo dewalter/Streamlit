77a90b2c653b3f62bda0317c7652464b15f46426
Walter <wkrijt@gmail.com>
2023-03-31 12:18:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,9 @@ col1, col2 = st.columns(2)
 col1.write('# This is Column 1')
 col2.write('# This is Column 2')
 
-import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
-df
