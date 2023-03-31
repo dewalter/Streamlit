2bb1c929d9c0e9a67a57534e1c9227f5386a4dc3
Walter <wkrijt@gmail.com>
2023-03-31 12:08:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pyplot as plt
 
-#read in the file
-movies_data = pd.read_csv("https://raw.githubusercontent.com/danielgrijalva/movie-stats/7c6a562377ab5c91bb80c405be50a0494ae8e582/movies.csv")
+st.balloons()st.progress(10)with st.spinner('Wait for it...'):    time.sleep(10)
 
 st.title('My First Streamlit App')
 st.text ('Hello everybody, my first text field in streamlit')
@@ -12,21 +10,3 @@ st.text ('Hello everybody, my first text field in streamlit')
 col1, col2 = st.columns(2)
 col1.write('# This is Column 1')
 col2.write('# This is Column 2')
-
-st.write("""
-Average Movie Budget, Grouped by Genre
-""")
-avg_budget = movies_data.groupby('genre')['budget'].mean().round()
-avg_budget = avg_budget.reset_index()
-genre = avg_budget['genre']
-avg_bud = avg_budget['budget']
-
-fig = plt.figure(figsize = (19, 10))
-
-plt.bar(genre, avg_bud, color = 'maroon')
-plt.xlabel('genre')
-plt.ylabel('budget')
-plt.title('Matplotlib Bar Chart Showing the Average \
-Budget of Movies in Each Genre')
-
-st.pyplot(fig)
