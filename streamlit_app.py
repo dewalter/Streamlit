dcb11bc174d26f0c138e4dc5cde2128dcd5d7efb
Walter <wkrijt@gmail.com>
2023-03-31 12:11:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.sidebar()
+st.sidebar('sidebar')
 
 st.title('My First Streamlit App')
 st.text ('Hello everybody, my first text field in streamlit')
@@ -10,4 +10,3 @@ st.text ('Hello everybody, my first text field in streamlit')
 col1, col2 = st.columns(2)
 col1.write('# This is Column 1')
 col2.write('# This is Column 2')
-
