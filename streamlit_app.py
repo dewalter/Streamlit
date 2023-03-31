1c8198ab84a37379a1821b88cabd63439dccdf56
Walter <wkrijt@gmail.com>
2023-03-31 12:21:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 
 st.container()
-
 st.title('My First Streamlit App')
 st.text ('Hello everybody, my first text field in streamlit')
+
 
 col1, col2 = st.columns(2)
 col1.write('# This is Column 1')
 col2.write('# This is Column 2')
+
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
@@ -17,8 +18,26 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 }))
 
+
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+
+# You can access the value at any point with:
+st.session_state.name
