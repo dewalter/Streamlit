009b049c9f7fa7993955247000dc8f6f0fe0fcf5
Walter <wkrijt@gmail.com>
2023-04-11 11:39:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import io
 import os
 import streamlit as st
-import PyPDF2
+import PyPDF
 
 st.title("PDF Compressor")
 
@@ -10,10 +10,10 @@ uploaded_file = st.file_uploader("Choose a PDF file to compress", type=["pdf"])
 
 if uploaded_file is not None:
     # Load PDF file into PyPDF2
-    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
+    pdf_reader = PyPDF.PdfFileReader(uploaded_file)
 
     # Create an empty output PDF file
-    output_pdf = PyPDF2.PdfFileWriter()
+    output_pdf = PyPDF.PdfFileWriter()
 
     # Loop over each page in the input PDF file
     for page_num in range(pdf_reader.getNumPages()):
@@ -24,7 +24,7 @@ if uploaded_file is not None:
         # Compress the page and write it to the output PDF file
         page.compressContentStreams()
         page.writeToStream(content)
-        output_pdf.addPage(PyPDF2.PdfFileReader(content).getPage(0))
+        output_pdf.addPage(PyPDF.PdfFileReader(content).getPage(0))
 
     # Create a temporary file to hold the compressed PDF
     temp_file = io.BytesIO()
