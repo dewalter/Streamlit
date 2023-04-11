9f2c7674543d98c37ce91debc408d91417b4698a
Walter <wkrijt@gmail.com>
2023-04-11 11:41:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,11 @@
-import io
-import os
-import streamlit as st
-import PyPDF
-
-st.title("PDF Compressor")
-
-# Allow user to upload a PDF file
-uploaded_file = st.file_uploader("Choose a PDF file to compress", type=["pdf"])
-
-if uploaded_file is not None:
-    # Load PDF file into PyPDF2
-    pdf_reader = PyPDF.PdfFileReader(uploaded_file)
-
-    # Create an empty output PDF file
-    output_pdf = PyPDF.PdfFileWriter()
-
-    # Loop over each page in the input PDF file
-    for page_num in range(pdf_reader.getNumPages()):
-        # Get the current page and its contents
-        page = pdf_reader.getPage(page_num)
-        content = io.BytesIO()
-
-        # Compress the page and write it to the output PDF file
-        page.compressContentStreams()
-        page.writeToStream(content)
-        output_pdf.addPage(PyPDF.PdfFileReader(content).getPage(0))
-
-    # Create a temporary file to hold the compressed PDF
-    temp_file = io.BytesIO()
-    output_pdf.write(temp_file)
-
-    # Display download link for compressed PDF
-    st.download_button(
-        label="Download Compressed PDF",
-        data=temp_file.getvalue(),
-        file_name=os.path.splitext(uploaded_file.name)[0] + "_compressed.pdf",
-        mime="application/pdf",
-    )
+def merge_pdf (pdfFiles):
+    merger = PdfFileMerger()
+    
+    for file in pdfFiles: 
+        merger.append(file, 'rb')
+        
+    _byteIo = BytesIO()
+    merger.write(_byteIo)    
+    _byteIo.seek(0)
+    
+    return _byteIo
